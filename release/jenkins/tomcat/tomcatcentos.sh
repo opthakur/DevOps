c067b5d27f11e50ba45cfebf8ccb883d0b455b99
roybhaskar9 <bhaskar@study9.com>
2018-06-24 08:41:09 +0000

    Update tomcatcentos.sh

--- release/jenkins/tomcat/tomcatcentos.sh
+++ release/jenkins/tomcat/tomcatcentos.sh
@@ -1,4 +1,4 @@
-sudo yum install -y tomcat tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc lynx
+sudo yum install -y tomcat tomcat-webapps tomcat-admin-webapps tomcat-docs-webapp tomcat-javadoc lynx wget
 sudo systemctl start tomcat
 sudo mv /usr/share/tomcat/conf/tomcat-users.xml /usr/share/tomcat/conf/tomcat-users.xml.bak
 sudo wget https://raw.githubusercontent.com/roybhaskar9/samplejava/master/tomcat-users-centos.xml -O /usr/share/tomcat/conf/tomcat-users.xml
