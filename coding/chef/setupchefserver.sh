712c9f01fa78cdff78da3b0329ba06902ca48b0b
roybhaskar9 <bhaskar@study9.com>
2018-07-11 03:12:35 +0000

    Add new file

---
+++ coding/chef/setupchefserver.sh
@@ -0,0 +1,8 @@
+wget https://packages.chef.io/files/stable/chef-server/12.17.33/ubuntu/16.04/chef-server-core_12.17.33-1_amd64.deb
+dpkg -i chef-server*.deb
+chef-server-ctl reconfigure
+chef-server-ctl install chef-manage
+chef-server-ctl reconfigure
+chef-manage-ctl reconfigure
+chef-server-ctl reconfigure
+chef-server-ctl user-create username Firstname Lastname username@chef.io 'abc123' --filename ~/username.pem
