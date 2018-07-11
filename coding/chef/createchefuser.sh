2cef73f05672114aee8ec3bea84b313a24188c35
roybhaskar9 <bhaskar@study9.com>
2018-07-11 03:06:21 +0000

    Update createchefuser.sh

--- coding/chef/createchefuser.sh
+++ coding/chef/createchefuser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-chef-server-ctl user-create username Firstname Lastname username@chef.io 'abc123' --filename ~/username.pem
+chef-server-ctl user-create chefuser Chef User chefuser@chef.io 'chefuser123' --filename ~/chefuser.pem
