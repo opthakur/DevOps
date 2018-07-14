7903507b918498fd785059e14e22c25c8f64dfc9
roybhaskar9 <bhaskar@study9.com>
2018-07-14 11:46:02 +0000

    Add new file

---
+++ coding/ruby.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/ruby -w
+# variables
+a = 20
+x = ["Blue", "Red", "Green", "Yellow", "White"]   
+# Operators
+puts(10 + 20) 
+# Print
+puts "Hello, Ruby!";
+# Conditional
+if a >= 18   
+  puts "You are eligible to vote."   
+else   
+  puts "You are not eligible to vote."   
+end  
+# Loop
+for i in x do   
+  puts i   
+end   
