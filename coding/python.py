747439e6ea20ed6f050c813ca331405d46badf6b
roybhaskar9 <bhaskar@study9.com>
2018-07-14 11:36:46 +0000

    Add new file

---
+++ coding/python.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+a = 21
+b = 10
+c = 0
+counter = 100   
+var = 100
+count = 0
+# Print Example
+print "Hello, Python!"
+# Variable Example
+print counter
+# Operator example
+c = a + b
+print "Line 1 - Value of c is ", c
+# Conditional Example
+if var == 200:
+   print "1 - Got a true expression value"
+   print var
+elif var == 150:
+   print "2 - Got a true expression value"
+   print var
+elif var == 100:
+   print "3 - Got a true expression value"
+   print var
+else:
+   print "4 - Got a false expression value"
+   print var
+   
+# Loop Example
+while (count < 9):
+   print 'The count is:', count
+   count = count + 1
