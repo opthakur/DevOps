728be417946fe473c7cc7eb898cf1aadc10602f2
roybhaskar9 <bhaskar@study9.com>
2018-07-14 12:18:18 +0000

    Update lampstack.pp

--- coding/puppet/puppetlamp/lampstack.pp
+++ coding/puppet/puppetlamp/lampstack.pp
@@ -1,3 +1,9 @@
+# execute 'apt-get update'
+exec { 'apt-update':                    # exec resource named 'apt-update'
+  command => '/usr/bin/apt-get update'  # command this resource will run
+}
+
+
 # install apache2 package
 package { 'apache2':
   ensure => installed,
