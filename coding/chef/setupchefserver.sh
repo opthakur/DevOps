b9ce74185845bcdfef271dfe8cf445101ea7e18e
roybhaskar9 <bhaskar@study9.com>
2018-07-14 14:36:20 +0000

    Update setupchefserver.sh

--- coding/chef/setupchefserver.sh
+++ coding/chef/setupchefserver.sh
@@ -1,10 +1,9 @@
 wget https://packages.chef.io/files/stable/chef-server/12.17.33/ubuntu/16.04/chef-server-core_12.17.33-1_amd64.deb
 dpkg -i chef-server*.deb
 chef-server-ctl reconfigure
+chef-server-ctl user-create chefuser Chef User chefuser@chef.io 'chefuser123' --filename ~/chefuser.pem
 wget https://packages.chef.io/files/stable/chef-manage/2.5.16/ubuntu/16.04/chef-manage_2.5.16-1_amd64.deb
 dpkg -i chef-manage*.deb
 #chef-server-ctl install chef-manage
 chef-server-ctl reconfigure
 chef-manage-ctl reconfigure
-chef-server-ctl reconfigure
-chef-server-ctl user-create chefuser Chef User chefuser@chef.io 'chefuser123' --filename ~/chefuser.pem
