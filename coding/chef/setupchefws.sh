7b74d976e081bc1ea16b7cd22dc57e0aa53a8041
roybhaskar9 <bhaskar@study9.com>
2018-09-01 11:22:16 +0000

    Update setupchefws.sh

--- coding/chef/setupchefws.sh
+++ coding/chef/setupchefws.sh
@@ -1,2 +1,5 @@
 wget https://packages.chef.io/files/stable/chefdk/3.1.0/ubuntu/16.04/chefdk_3.1.0-1_amd64.deb
 dpkg -i chefdk*.deb
+cd /etc
+mv hosts hosts.bak
+https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/hosts
