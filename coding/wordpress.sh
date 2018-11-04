1ad0c90770a46c47561c31436bcd20dc69fc0b66
roybhaskar9 <bhaskar@study9.com>
2018-11-04 17:28:34 +0000

    Update wordpress.sh

--- coding/wordpress.sh
+++ coding/wordpress.sh
@@ -8,8 +8,9 @@ sudo cp mysqlcommands /tmp/mysqlcommands
 mysql -uroot -prootpassword < /tmp/mysqlcommands
 wget https://wordpress.org/latest.zip
 sudo apt install -y unzip
+cp latest.zip /tmp/latest.zip
 sudo unzip /tmp/latest.zip -d /var/www/html
 wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/chefwordpress/files/default/wp-config-sample.php
 sudo cp wp-config-sample.php /var/www/html/wordpress/wp-config.php
 sudo chmod -R 775 /var/www/html/wordpress
-sudo chown -R apache:apache /var/www/html/wordpress
+sudo chown -R www-data:www-data /var/www/html/wordpress
