e63cd640ab5481341e78f8b04fcc04877c760d25
roybhaskar9 <bhaskar@study9.com>
2018-11-19 04:01:57 +0000

    Update s9labsetup.bat

--- Lab/LaptopLab/Windows/version10/s9labsetup.bat
+++ Lab/LaptopLab/Windows/version10/s9labsetup.bat
@@ -1,6 +1,6 @@
 @powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install -y wget nano consolez notepad2
+choco install -y wget nano cmder notepad2
 
 choco install -y virtualbox
 
