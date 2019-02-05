215ee8c2c6929da12c60a56e36bb44308afd5861
roybhaskar9 <bhaskar@study9.com>
2019-02-05 02:39:40 +0000

    Update setupchefnode.sh

--- coding/chef/setupchefnode.sh
+++ coding/chef/setupchefnode.sh
@@ -1,6 +1,6 @@
 cd /etc
 mv hosts hosts.bak
-https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/hosts
+wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/hosts
 cd ~
 sudo sed -i '28s/prohibit-password/yes/' /etc/ssh/sshd_config
 sudo sed -i '52s/no/yes/' /etc/ssh/sshd_config
