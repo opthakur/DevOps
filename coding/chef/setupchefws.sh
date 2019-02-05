b55317359b038a7ed9749256c12e265d67d3702f
roybhaskar9 <bhaskar@study9.com>
2019-02-05 02:41:16 +0000

    Update setupchefws.sh

--- coding/chef/setupchefws.sh
+++ coding/chef/setupchefws.sh
@@ -2,7 +2,7 @@ wget https://packages.chef.io/files/stable/chefdk/3.1.0/ubuntu/16.04/chefdk_3.1.
 dpkg -i chefdk*.deb
 cd /etc
 mv hosts hosts.bak
-https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/hosts
+wget https://gitlab.com/roybhaskar9/devops/raw/master/coding/chef/hosts
 cd ~
 cp /vagrant/chef-starter.zip .
 apt install -y unzip
