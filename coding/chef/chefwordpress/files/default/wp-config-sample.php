64c4b101b494d784abfb4747eb73ab68117e4529
roybhaskar9 <bhaskar@study9.com>
2019-04-20 14:40:33 +0000

    Update wp-config-sample.php

--- coding/chef/chefwordpress/files/default/wp-config-sample.php
+++ coding/chef/chefwordpress/files/default/wp-config-sample.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'wordpress');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'wordpressuser');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'rootpassword');
+define('DB_PASSWORD', 'password');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
