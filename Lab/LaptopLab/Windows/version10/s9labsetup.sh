a241b0124fd717797b036343318cba829021b5f7
roybhaskar9 <bhaskar@study9.com>
2019-05-05 09:46:25 +0000

    Add new file

---
+++ Lab/LaptopLab/Windows/version10/s9labsetup.sh
@@ -0,0 +1,21 @@
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y wget nano notepad2
+
+choco install -y consolez cmder conemu 
+
+choco install -y virtualbox
+
+choco install -y vagrant
+
+choco install -y git --params "/GitAndUnixToolsOnPath /WindowsTerminal"
+
+refreshenv
+
+bash
+
+cd ~
+
+curl -sSL https://raw.githubusercontent.com/roybhaskar9/files/master/setupvagrant.sh | bash
+
+curl -sSL https://gitlab.com/roybhaskar9/devops/raw/master/Lab/LaptopLab/Windows/version10/labfolders.sh | bash
