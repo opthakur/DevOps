08a92f71593d4fdd726f5029119b325bbe247d76
roybhaskar9 <bhaskar@study9.com>
2019-06-10 02:51:44 +0000

    Update default.rb

--- coding/chef/chefcookbooks/cookbooks/starter/recipes/default.rb
+++ coding/chef/chefcookbooks/cookbooks/starter/recipes/default.rb
@@ -1,6 +1,10 @@
 # This is a Chef recipe file. It can be used to specify resources which will
 # apply configuration to a server.
 
+log "Welcome to chef #{node["starter_name"]!" do
+  level :info
+end
+
 package "apache2" do
   action :install
 end
